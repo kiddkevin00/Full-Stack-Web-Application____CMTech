ce3d31e91658542dcfbccf09bc530dbe40010f53
Qi Tang <qitang@Qis-MacBook-Air.local>
2015-02-12 15:07:42 -0500

    added home tab

--- client/app/project/project.js
+++ client/app/project/project.js
@@ -1,18 +1,22 @@
 'use strict';
 
 angular.module('qiApp')
-  .config(function ($stateProvider) {
+  .config(function ($stateProvider,$urlRouterProvider) {
     $stateProvider
       .state('project', {
         url: '/project',
         templateUrl: 'app/project/project.html',
         controller: 'ProjectCtrl'
       }).state('detail', {
-      	url: '/project/:projectId',
+      	url: '/project',
+      	abstract: true,
       	templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       }).state('detail.report', {
-      	url: '/report',
+      	url: '/:projectId',
       	templateUrl : '/components/tabs/daily_report.html',
+      }).state('detail.home', {
+      	url: '/:projectId',
+      	templateUrl : '/components/tabs/home.html',
       });
   });
