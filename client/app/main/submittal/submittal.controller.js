3533067d887ab87fe1ed5396dd3795e5ec698102
MARCUS HSU <mh3346@columbia.edu>
2015-02-14 00:22:09 -0500

    Added the view of create submittal form

--- client/app/main/submittal/submittal.controller.js
+++ client/app/main/submittal/submittal.controller.js
@@ -2,4 +2,11 @@
 
 angular.module('qiApp').controller('SubmittalCtrl', function ($scope, $location) {
     $scope.projectID = $location.path().substring($location.path().indexOf("/", 1) + 1);
+    $scope.isCreatingSubmittal = false;
+    $scope.createSubmittal = function() {
+        $scope.isCreatingSubmittal = true;
+    };
+    $scope.leaveCreateSubmittal = function() {
+        $scope.isCreatingSubmittal = false;
+    }
 });
