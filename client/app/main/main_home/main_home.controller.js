061bcf42b629d2db55a3d9924c62a5942d243952
Qi Tang <qitang@Qis-MacBook-Air.local>
2015-03-22 17:30:18 -0400

    sdsdsdsdsdsdsdsdsdsdsdsdsds

--- client/app/main/main_home/main_home.controller.js
+++ client/app/main/main_home/main_home.controller.js
@@ -3,4 +3,5 @@
 angular.module('qiApp').controller('MainHomeCtrl', function ($scope, $stateParams, $http, $rootScope) {
     //$scope.projectID = $stateParams.projectID;
     	//$rootScope.project ={ _id: $scope.projectID};
+    
 });
